317cd57742bda2aef64936c6eb7dff42db5f4bbb
mostafa <mohammed.mostufa@yahoo.com>
2018-03-09 18:02:37 +0200

    The code for scoreTeamA

---
+++ app/src/main/java/com/example/andriod/courtcounter/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.andriod.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /**
+     * This method is called when the order + is clicked.
+     */
+    public void AddThreePoints(View view) {
+
+        scoreTeamA = scoreTeamA + 3 ;
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * This method is called when the order + is clicked.
+     */
+    public void AddTwoPoints(View view) {
+
+        scoreTeamA = scoreTeamA + 2 ;
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * This method is called when the order + is clicked.
+     */
+    public void AddOnePoint(View view) {
+
+        scoreTeamA = scoreTeamA + 1 ;
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
