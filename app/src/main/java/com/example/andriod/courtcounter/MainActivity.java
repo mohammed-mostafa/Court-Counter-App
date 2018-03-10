7839a92f54e3fb5a74e45a317a375ac731504aa8
mostafa <mohammed.mostufa@yahoo.com>
2018-03-10 03:42:03 +0200

    Add the code for team B there are two teams now

--- app/src/main/java/com/example/andriod/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/andriod/courtcounter/MainActivity.java
@@ -6,37 +6,42 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int scoreTeamA = 0 ;
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     /**
      * This method is called when the order + is clicked.
      */
     public void AddThreePoints(View view) {
 
-        scoreTeamA = scoreTeamA + 3 ;
+        scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
+
     /**
      * This method is called when the order + is clicked.
      */
     public void AddTwoPoints(View view) {
 
-        scoreTeamA = scoreTeamA + 2 ;
+        scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
+
     /**
      * This method is called when the order + is clicked.
      */
     public void AddOnePoint(View view) {
 
-        scoreTeamA = scoreTeamA + 1 ;
+        scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
+
     /**
      * Displays the given score for Team A.
      */
@@ -44,4 +49,40 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
+    /**
+     * This method is called when the order + is clicked.
+     */
+    public void AddThreePointsB(View view) {
+
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method is called when the order + is clicked.
+     */
+    public void AddTwoPointsB(View view) {
+
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method is called when the order + is clicked.
+     */
+    public void AddOnePointB(View view) {
+
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
